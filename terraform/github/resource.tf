3915896f10e91761a8d89956ba0bf452637b4726
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-09-29 09:04:23 +0545

    add: github resources for basic operations

---
+++ terraform/github/resource.tf
@@ -0,0 +1,12 @@
+resource "github_repository" "terraform" {
+  name = "terraform"
+  description = "This repo is created by using terraform. "
+
+  visibility = "private"
+}
+
+resource "github_repository_collaborator" "terraform" {
+  repository = "terraform"
+  username   = "dwdraju"
+  permission = "admin"
+}
