6603c0ef2161833e9d137e935e00b0c8be3a5a69
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-10-19 21:29:53 +0545

    fix: variables for the ec2 instance

--- terraform/aws/modules/ec2/variables.tf
+++ terraform/aws/modules/ec2/variables.tf
@@ -1,1 +1,38 @@
-variable "public_subnet" {}
+variable "ami" {
+  type        = string
+  description = "AMI"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance Type"
+}
+
+
+variable "public_subnet" {
+  type        = string
+  description = "Public Subnet"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "CIDR Block"
+}
+
+#variable "security_groups" {
+#  type        = list(string)
+#  description = "Security Group for EC2"
+#}
+
+variable "vpc_security_group_ids" {
+  type        = list(string)
+  description = "Security Group ID"
+}
+
+variable "associate_public_ip_address" {}
+variable "subnet_id" {}
