4068ace406e926471b78ea8b3c5b574f7ade4814
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-10-19 21:28:38 +0545

    refactor: remove the vpc resources

--- terraform/aws/modules/vpc/subnet.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "publicsubnet" {
-  vpc_id     = aws_vpc.example_vpc.id
-  cidr_block = var.public_subnet_cidr
-
-  tags = {
-    project = "Example"
-  }
-}
--- terraform/aws/modules/vpc/vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "example_vpc" {
-  cidr_block = var.cidr_block
-
-  tags = {
-    project = "Example"
-  }
-}
