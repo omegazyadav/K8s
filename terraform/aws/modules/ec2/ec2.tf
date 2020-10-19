3f6bc297be73a9e7eb25ffe10638af5e2ac1241b
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-10-19 21:29:12 +0545

    fix: ec2 module definition

--- terraform/aws/modules/ec2/ec2.tf
+++ terraform/aws/modules/ec2/ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "instance" {
-  ami           = "ami-06e54d05255faf8f6"
-  instance_type = "t2.micro"
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+  subnet_id              = var.subnet_id
 
   tags = {
     project = "Example"
