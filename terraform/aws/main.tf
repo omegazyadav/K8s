ddf1a3fab20640225a6c0b8ba071afeebc7b6143
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-10-28 18:41:07 +0545

    fix: restructure the module

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,51 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "YOUR_ACCESS_KEY"
-  secret_key = "YOUR_SECRET_KEY"
-}
-
-# Confiugre the VPC
-resource "aws_vpc" "example_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "Example"
-  }
-}
-
-
-# Configure the subent
-resource "aws_subnet" "example_subnet" {
-  vpc_id     = aws_vpc.example_vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "Example"
-  }
-}
-
-## Security Group
+# Security Group
 resource "aws_security_group" "example_sg" {
   name        = "ExampleSG"
   description = "Allow ssh inbound traffic"
-  vpc_id      = aws_vpc.example_vpc.id
+  vpc_id      = module.example_ec2.vpc_id
 
   ingress {
     description = "Allow SSH connection"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_subnet.example_subnet.cidr_block]
+    cidr_blocks = [module.example_ec2.cidr_block]
   }
 
   tags = {
@@ -57,12 +21,11 @@ resource "aws_security_group" "example_sg" {
 module "example_ec2" {
   source = "./modules/ec2"
 
-  ami                         = "ami-01fee56b22f308154"
+  ami                         = "ami-07efac79022b86107"
   instance_type               = "t2.micro"
-  vpc_id                      = aws_security_group.example_sg.id
-  subnet_id                   = aws_subnet.example_subnet.id
-  cidr_block                  = aws_subnet.example_subnet.cidr_block
+  vpc_id                      = module.example_ec2.vpc_id
+  subnet_id                   = module.example_ec2.public_subnet_id
+  cidr_block                  = module.example_ec2.cidr_block
   vpc_security_group_ids      = [aws_security_group.example_sg.id]
   associate_public_ip_address = true
 }
-
