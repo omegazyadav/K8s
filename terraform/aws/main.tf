6f2ae63d1e1567c2b4f0d1d27bc4a54bf93b7a79
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-10-31 20:19:42 +0545

    fix: rename the subnet details

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -4,7 +4,7 @@ module "example_ec2" {
 
   ami                         = "ami-07efac79022b86107"
   instance_type               = "t2.micro"
-  subnet_id                   = module.example_ec2.public_subnet_id
+  subnet_id                   = module.example_ec2.subnet_id
   vpc_security_group_ids      = [module.example_ec2.security_group]
-  associate_public_ip_address = true
+  associate_public_ip_address = "true"
 }
