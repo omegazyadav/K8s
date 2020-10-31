9cb1976bc53cb5b5889623e1bda794c029ae354b
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-10-31 06:41:53 +0545

    refactor: remove the unwanted block like vpc_id, cidr_block from main module

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -4,9 +4,7 @@ module "example_ec2" {
 
   ami                         = "ami-07efac79022b86107"
   instance_type               = "t2.micro"
-  vpc_id                      = module.example_ec2.vpc_id
   subnet_id                   = module.example_ec2.public_subnet_id
-  cidr_block                  = module.example_ec2.cidr_block
   vpc_security_group_ids      = [module.example_ec2.security_group]
   associate_public_ip_address = true
 }
