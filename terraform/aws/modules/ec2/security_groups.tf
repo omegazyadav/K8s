1d2426406f3c05e6966d0a60fa42457a114bab99
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-10-31 06:43:00 +0545

    add: egress information for the aws instance

--- terraform/aws/modules/ec2/security_groups.tf
+++ terraform/aws/modules/ec2/security_groups.tf
@@ -2,17 +2,24 @@
 resource "aws_security_group" "example_sg" {
   name        = "ExampleSG"
   description = "Allow ssh inbound traffic"
-  vpc_id      = var.vpc_id
+  vpc_id      = aws_vpc.example_vpc.id
 
   ingress {
     description = "Allow SSH connection"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.cidr_block]
+    cidr_blocks = [aws_subnet.example_subnet.cidr_block]
   }
 
-  tags = { 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
     Name = "ExampleSG"
   }
 }
