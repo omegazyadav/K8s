b0d3d4881b5ab4e548ac8cdaa570b3c67a71c63f
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-10-31 20:20:30 +0545

    add: network interface resources

--- terraform/aws/modules/ec2/ec2.tf
+++ terraform/aws/modules/ec2/ec2.tf
@@ -1,8 +1,11 @@
 resource "aws_instance" "instance" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  vpc_security_group_ids = var.vpc_security_group_ids
-  subnet_id              = var.subnet_id
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  network_interface {
+    network_interface_id = aws_network_interface.defaultNIC.id
+    device_index         = 0
+  }
 
   tags = {
     project = "Example"
