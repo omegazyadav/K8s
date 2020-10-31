dbea64364d6269ef48d32a00c0bebef4691898c2
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-10-31 06:42:39 +0545

    refactor: remove the cidr_block and vpc_id details

--- terraform/aws/modules/ec2/outputs.tf
+++ terraform/aws/modules/ec2/outputs.tf
@@ -2,14 +2,6 @@ output "public_subnet_id" {
   value = aws_subnet.example_subnet.id
 }
 
-output "vpc_id" {
-  value = aws_vpc.example_vpc.id
-}
-
-output "cidr_block" {
-  value = aws_subnet.example_subnet.cidr_block
-}
-
 output "security_group" {
   value = aws_security_group.example_sg.id
 }
