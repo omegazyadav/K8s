40ea8f88028f8e87acfbbc6b0b85595bd923fb42
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-11-02 08:38:23 +0545

    add: ssh public key resource

--- terraform/aws/modules/ec2/ec2.tf
+++ terraform/aws/modules/ec2/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name      = aws_key_pair.public_key.key_name
 
   network_interface {
     network_interface_id = aws_network_interface.defaultNIC.id
