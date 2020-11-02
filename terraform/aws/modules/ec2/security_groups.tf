9765156994acfe100f76b5452db0b02e83ad8052
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-11-02 08:33:41 +0545

    add: inbound and outbound rules for the security group

--- terraform/aws/modules/ec2/security_groups.tf
+++ terraform/aws/modules/ec2/security_groups.tf
@@ -9,7 +9,23 @@ resource "aws_security_group" "example_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_subnet.example_subnet.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
