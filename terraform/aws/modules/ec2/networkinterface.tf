576ae199badc34d3348c43651137bdfb25b7d6e5
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-11-02 08:39:29 +0545

    add: association rules for security group with network interface

--- terraform/aws/modules/ec2/networkinterface.tf
+++ terraform/aws/modules/ec2/networkinterface.tf
@@ -2,8 +2,11 @@ resource "aws_network_interface" "defaultNIC" {
   subnet_id = aws_subnet.example_subnet.id
 
   tags = {
-    project    = "Collabnix"
-    department = "Automation"
+    Name = "Example"
   }
 }
 
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.example_sg.id
+  network_interface_id = aws_instance.instance.primary_network_interface_id
+}
