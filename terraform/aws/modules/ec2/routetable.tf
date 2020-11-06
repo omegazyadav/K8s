89021c58e70d30d59d9e7c8bb512b781394db2a6
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-11-07 04:49:53 +0545

    add: association of main route table

--- terraform/aws/modules/ec2/routetable.tf
+++ terraform/aws/modules/ec2/routetable.tf
@@ -11,3 +11,8 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_main_route_table_association" "main_route" {
+  vpc_id         = aws_vpc.example_vpc.id
+  route_table_id = aws_route_table.public.id
+}
+
