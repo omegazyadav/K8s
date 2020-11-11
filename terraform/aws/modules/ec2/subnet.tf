6a3ee8ee86da0a3b45fcc3676b6e22f17d741eea
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-11-11 22:52:55 +0545

    fix: subnet issue with different cidr

--- terraform/aws/modules/ec2/subnet.tf
+++ terraform/aws/modules/ec2/subnet.tf
@@ -19,11 +19,21 @@ resource "aws_subnet" "public_subnet" {
 }
 
 # Configure the private subnet 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "private_subnet1" {
   vpc_id     = aws_vpc.example_vpc.id
   cidr_block = "10.0.2.0/24"
 
   tags = {
-    Name = "Private"
+    Name = "Private1"
+  }
+}
+
+# Configure the private subnet 
+resource "aws_subnet" "private_subnet2" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "Private2"
   }
 }
