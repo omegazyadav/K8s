5c0fc514c16e5d5a8b6a1a631bf30e42b04030fd
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-11-15 14:40:25 +0545

    add: inbound rules for the RDS

--- terraform/aws/modules/rds/security_groups.tf
+++ terraform/aws/modules/rds/security_groups.tf
@@ -13,3 +13,12 @@ resource "aws_security_group_rule" "inbound_access" {
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "inbound_ssh" {
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.rds_sg.id
+  to_port           = 22
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
