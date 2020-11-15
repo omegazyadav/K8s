ee1a3ea19facf66ebfcff6ae43afe9ddd47b7d03
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-11-15 14:41:11 +0545

    add: accept connection for ghost application

--- terraform/aws/modules/ec2/security_groups.tf
+++ terraform/aws/modules/ec2/security_groups.tf
@@ -22,6 +22,14 @@ resource "aws_security_group" "example_sg" {
 
   ingress {
     description = "HTTP"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
