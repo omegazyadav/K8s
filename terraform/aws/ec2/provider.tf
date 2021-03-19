77b2a3e5770082dd39c8c351acfdd252b904d537
Yadav Lamichhane <omegazyadav1@gmail.com>
2021-03-19 22:17:03 +0545

    refactor: basic cleanup with provider and key pair

--- terraform/aws/ec2/provider.tf
+++ terraform/aws/ec2/provider.tf
@@ -1,18 +1,14 @@
 # Terraform configuration
 terraform {
   required_providers {
-    aws = { 
+    aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
-    } 
+    }
   }
 }
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-2"
-  #access_key = "YOUR_ACCESS_KEY"
-  #secret_key = "YOUR_SECRET_KEY"
-  shared_credentials_file = "/home/omega/.aws/credentials"
-  profile                 = "yadav"
 }
 
