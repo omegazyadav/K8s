27619448679e40edd68d1d8180ec8f2ab528d45f
Yadav Lamichhane <omegazyadav1@gmail.com>
2021-03-19 22:25:02 +0545

    refactor: basic cleanup in backend and provider secion

--- terraform/aws/s3/backend.tf
+++ terraform/aws/s3/backend.tf
@@ -1,9 +1,10 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
 terraform {
   backend "s3" {
     region = "us-west-2"
-    shared_credentials_file = "~/.aws/credentials"
-    profile = "gkerocks"
-
     bucket = "omegazyadav-2020-12-21"
     key    = "terraform.tfstate"
     dynamodb_table = "omega"
--- terraform/aws/s3/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region = "us-west-2"
-  shared_credentials_file = "/home/omega/.aws/credentials"
-  profile = "gkerocks"
-}
